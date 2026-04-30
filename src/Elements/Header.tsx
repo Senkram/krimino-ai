ec4cc38ef173c5833b681a99ff716e1eb8fd1711
Kadir Tuna Yiğit <kadirtunayigit@hotmail.com>
2026-04-30 18:18:13 +0300

    Veri çekebiliyoruz

---
+++ src/Elements/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <div className="w-full h-40 bg-blue-600"></div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,18 @@ import Form from "@/Elements/Form";
 import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
-  const olaylar = await prisma.analiz_sonuclari.findMany();
+  const analiz_sonuclari = await prisma.analiz_sonuclari.findMany();
   return (
     <div className="flex flex-1 bg-violet-700 flex-col min-h-full p-40 justify-evenly">
       <AiChat aiText=""></AiChat>
       <div>
-        {olaylar.map((olay: any) => (
-          <li key={olay.id} className="p-4 border rounded shadow-sm">
-            <span className="font-semibold">{olay.olay_turu}</span> -{" "}
-            {olay.konum}
-            <p className="text-gray-600 text-sm">{olay.aciklama}</p>
+        {analiz_sonuclari.map((sonuc: any) => (
+          <li key={sonuc.id} className="p-4 border rounded shadow-sm">
+            <span className="font-semibold">
+              {sonuc.olusturulma_tarihi.toString()}
+            </span>
+            - {sonuc.id}
+            <p className="text-gray-600 text-sm">{sonuc.supheli_id}</p>
           </li>
         ))}
       </div>
